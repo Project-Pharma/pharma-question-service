b644c886f5af15e9e60fb9e1469f092f050de848
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2020-08-08 15:21:44 -0700

    Initial Add

---
+++ src/main/java/com/inoastrum/pharmaquestionservice/PharmaQuestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inoastrum.pharmaquestionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaQuestionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaQuestionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inoastrum/pharmaquestionservice/PharmaQuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inoastrum.pharmaquestionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaQuestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
